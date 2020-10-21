6f2b1ff54f2a1f3bb5a77515c9daeadd4d44d178
farrriq <72791465+farrriq@users.noreply.github.com>
2020-10-21 09:01:46 +0700

    Update fb-add-friend.js

--- fb-add-friend.js
+++ fb-add-friend.js
@@ -10,12 +10,12 @@
 
 ! function() {
     "use strict";
-    var e = 1;
+    var e = 3;
     document.getElementById("fbSearchResultsBox").scrollIntoView(!0);
     var l = setInterval(function() {
             var e, l = document.querySelectorAll(".layerCancel");
             for (e = 0; e < l.length; ++e) l[e].click()
-        }, 500),
+        }, 1500),
         t = setInterval(function() {
             var n = document.querySelectorAll(".FriendRequestAdd");
             e < n.length ? (n[e].scrollIntoView(!0), window.scrollBy(0, 96.5), n[e].click(), e++) : (clearInterval(t), clearInterval(l), window.location.reload(!1))
